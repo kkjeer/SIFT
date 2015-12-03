eb9c746a27acfc6fa5d05d699420e060e93a254f
kkjeer <kkjeer@wellesley.edu>
2015-12-03 14:04:55 -0500

    Replaced cubes with books

--- Book.js
+++ Book.js
@@ -1,16 +1,19 @@
-function Book (width, height, depth, color) {
+function Book (width, height, depth, color, name) {
 	//properties from parameters
 	this.width = width;
 	this.height = height;
 	this.depth = depth;
 	this.color = color;
+	this.name = name;
 
 	//page properties
 	this.pageColor = 0xffefdb;
+	this.pageWidth = 0.9 * this.width;
 	this.numPages = 10;
 
 	//frame
 	this.frame = new THREE.Object3D();
+	this.frame.name = this.name;
 	this.addInnerFrame();
 }
 
@@ -20,6 +23,7 @@ function Book (width, height, depth, color) {
 //z-axis by 0.5 * this.depth in each direction
 Book.prototype.addInnerFrame = function () {
 	this.innerFrame = new THREE.Object3D();
+	this.innerFrame.name = this.name + 'InnnerFrame';
 
 	var front = this.makeFrontCover();
 	front.position.set(0, 0, 0.5 * this.depth);
@@ -27,7 +31,11 @@ Book.prototype.addInnerFrame = function () {
 
 	var back = this.makeBackCover();
 	back.position.set(0, 0, -0.5 * this.depth);
-	this.innerFrame.add(front);
+	this.innerFrame.add(back);
+
+	var spine = this.makeSpine();
+	spine.rotateY(-Math.PI/2);
+	this.innerFrame.add(spine);
 
 	this.addPages();
 
@@ -38,13 +46,15 @@ Book.prototype.addInnerFrame = function () {
 //extends along x and y axes
 Book.prototype.makeFrontCover = function () {
 	var frontCoverFrame = new THREE.Object3D();
+	frontCoverFrame.name = this.name + 'FrontCoverFrame';
 
 	var frontGeom = new THREE.PlaneGeometry(this.width, this.height);
-	var frontMat = new THREE.MeshPhongMaterial({color: this.color, ambient: this.color});
-	var front = new THREE.Mesh(frontGeom, frontMat);
+	var frontMat = new THREE.MeshPhongMaterial({color: this.color, ambient: this.color, side: THREE.DoubleSide});
+	this.front = new THREE.Mesh(frontGeom, frontMat);
+	this.front.name = this.name + 'FrontCoverMesh';
 
-	front.position.set(0.5 * this.width, 0, 0);
-	frontCoverFrame.add(front);
+	this.front.position.set(0.5 * this.width, 0, 0);
+	frontCoverFrame.add(this.front);
 
 	return frontCoverFrame;
 }
@@ -53,19 +63,59 @@ Book.prototype.makeFrontCover = function () {
 //extends along x and y axes
 Book.prototype.makeBackCover = function () {
 	var backCoverFrame = new THREE.Object3D();
+	backCoverFrame.name = this.name + 'BackCoverFrame';
 
 	var backGeom = new THREE.PlaneGeometry(this.width, this.height);
-	var backMat = new THREE.MeshPhongMaterial({color: this.color, ambient: this.color});
-	var back = new THREE.Mesh(backGeom, backMat);
+	var backMat = new THREE.MeshPhongMaterial({color: this.color, ambient: this.color, side: THREE.DoubleSide});
+	this.back = new THREE.Mesh(backGeom, backMat);
+	this.back.name = this.name + 'BackCoverMesh';
 
-	back.position.set(0.5 * this.width, 0, 0);
-	backCoverFrame.add(back);
+	this.back.position.set(0.5 * this.width, 0, 0);
+	backCoverFrame.add(this.back);
 
 	return backCoverFrame;
 }
 
+//origin: bottom left corner
+//extends along position x and y axes, curves in positive z-direction
 Book.prototype.makeSpine = function () {
+	this.spineCurvature = 0.75 * this.depth;
 	var spineFrame = new THREE.Object3D();
+	spineFrame.name = this.name + 'SpineFrame';
+
+	var spinePoints = [
+		[
+			[0, 0, 0],
+			[0.25 * this.depth, 0, 0.5 * this.spineCurvature],
+			[0.75 * this.depth, 0, 0.5 * this.spineCurvature],
+			[this.depth, 0, 0]
+		],
+		[
+			[0, 0.25 * this.height, 0],
+			[0.25 * this.depth, 0.25 * this.height, 0.5 * this.spineCurvature],
+			[0.75 * this.depth, 0.25 * this.height, 0.5 * this.spineCurvature],
+			[this.depth, 0.25 * this.height, 0]
+		],
+		[
+			[0, 0, 0],
+			[0.25 * this.depth, 0.75 * this.height, 0.5 * this.spineCurvature],
+			[0.75 * this.depth, 0.75 * this.height, 0.5 * this.spineCurvature],
+			[this.depth, 0.75 * this.height, 0]
+		],
+		[
+			[0, this.height, 0],
+			[0.25 * this.depth, this.height, 0.5 * this.spineCurvature],
+			[0.75 * this.depth, this.height, 0.5 * this.spineCurvature],
+			[this.depth, this.height, 0]
+		]
+	];
+	var spineGeom = new THREE.BezierSurfaceGeometry(spinePoints, 20, 20);
+	var spineMat = new THREE.MeshPhongMaterial({color: this.color, ambient: this.color, side: THREE.DoubleSide});
+	this.spine = new THREE.Mesh(spineGeom, spineMat);
+	this.spine.name = this.name + 'SpineMesh';
+
+	this.spine.position.set(-0.5 * this.depth, -0.5 * this.height, 0);
+	spineFrame.add(this.spine);
 
 	return spineFrame;
 }
@@ -85,17 +135,22 @@ Book.prototype.addPages = function () {
 //extends along x and y axes
 Book.prototype.makePage = function () {
 	var pageFrame = new THREE.Object3D();
+	pageFrame.name = this.name + 'PageFrame';
 
-	var pageGeom = new THREE.PlaneGeometry(this.width, this.height);
+	var pageGeom = new THREE.PlaneGeometry(this.pageWidth, this.height);
 	var pageMat = new THREE.MeshPhongMaterial({color: this.pageColor, ambient: this.pageColor});
 	var page = new THREE.Mesh(pageGeom, pageMat);
+	page.name = this.name + 'PageMesh';
 
-	page.position.set(0.5 * this.width, 0, 0);
+	page.position.set(0.5 * this.pageWidth, 0, 0);
 	pageFrame.add(page);
 
 	return pageFrame;
 }
 
-Book.prototype.flipPages = function () {
-
+Book.prototype.highlight = function () {
+	this.highlightColor = 0xffff00;
+	this.front.material.emissive.setHex(this.highlightColor);
+	this.back.material.emissive.setHex(this.highlightColor);
+	this.spine.material.emissive.setHex(this.highlightColor);
 }
