09a7c2196d757432899f369bfd3b960d56c1bc8b
kkjeer <kkjeer@wellesley.edu>
2015-12-03 14:44:32 -0500

    Added shelf with books

--- Shelf.js
+++ Shelf.js
@@ -1,3 +1,78 @@
-function Shelf () {
-	
+function Shelf (width, height, depth, name) {
+	this.width = width;
+	this.height = height;
+	this.depth = depth;
+	this.name = name;
+	this.color = 0x8b3626;
+
+	this.frame = new THREE.Object3D();
+	this.addInnerFrame();
+}
+
+Shelf.prototype.addInnerFrame = function () {
+	this.innerFrame = new THREE.Object3D();
+
+	var flat = this.makeFlatShelf();
+	this.innerFrame.add(flat);
+
+	this.frame.add(this.innerFrame);
+}
+
+Shelf.prototype.makeFlatShelf = function () {
+	this.flatThickness = 0.2 * this.height;
+	var flatFrame = new THREE.Object3D();
+	flatFrame.name = this.name + 'FlatFrame';
+
+	var flatGeom = new THREE.CubeGeometry(this.width, this.flatThickness, this.depth);
+	var flatMat = new THREE.MeshPhongMaterial({color: this.color, ambient: this.color});
+	var flat = new THREE.Mesh(flatGeom, flatMat);
+	flat.name = this.name + 'FlatMesh';
+
+	flatFrame.add(flat);
+
+	return flatFrame;
+}
+
+Shelf.prototype.addBooks = function (numBooks, scene, objects, objectsControls) {
+	var shelfPos = this.frame.position;
+	for (var i = 0; i < numBooks; i++) {
+		var bookWidth = this.depth;
+		var bookHeight = (Math.random() * 3 + 4) * this.flatThickness;
+		var bookDepth = this.width/numBooks;
+		var bookSpacing = 0.3 * bookDepth;
+		var book = new Book(bookWidth, bookHeight, (Math.random() * 0.5 + 0.5) * (bookDepth - bookSpacing), Math.random() * 0xffffff, 'book' + i);
+		var object = book.frame;
+		object.position.copy(shelfPos);
+		object.position.x -= 0.5 * this.width;
+		object.position.x += (i + 0.75) * bookDepth;
+		object.position.y += this.flatThickness + 0.5 * bookHeight;
+		object.position.z += 0.5 * this.depth;
+		object.rotateY(Math.PI/2);
+
+		// leap object controls
+    var objectControls = new THREE.LeapObjectControls(camera, object);
+
+    objectControls.rotateEnabled  = true;
+    objectControls.rotateSpeed    = 3;
+    objectControls.rotateHands    = 1;
+    objectControls.rotateFingers  = [2, 3];
+    
+    // *******Get rid of the scale function on objects******** 
+    // *******Set objectControls.scaleEnabled = false
+     objectControls.scaleEnabled   = false;
+    objectControls.scaleSpeed     = 3;
+    objectControls.scaleHands     = 1;
+    objectControls.scaleFingers   = [4, 5];
+
+    
+    objectControls.panEnabled     = true;
+    objectControls.panSpeed       = 3;
+    objectControls.panHands       = 2;
+    objectControls.panFingers     = [6, 12];
+    objectControls.panRightHanded = false; // for left-handed person
+
+    scene.add(object);
+    objects.push(object);
+    objectsControls.push(objectControls);
+	}
 }
