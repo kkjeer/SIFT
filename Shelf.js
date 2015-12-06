9da3cd74dad8a2aaea6bf176057182f069b0cd2a
kkjeer <kkjeer@wellesley.edu>
2015-12-06 16:24:02 -0500

    Intersected books can be added to the shelf

--- Shelf.js
+++ Shelf.js
@@ -22,6 +22,10 @@ Shelf.prototype.addInnerFrame = function () {
 	this.frame.add(this.innerFrame);
 }
 
+//origin: center of box
+//extends in x by -0.5 * this.width, 0.5 * this.width
+//extends in y by -0.5 * this.flatThickness, 0.5 * this.flatThickness
+//extends in z by -0.5 * this.depth, 0.5 * this.depth
 Shelf.prototype.makeFlatShelf = function () {
 	this.flatWidth = 1.1 * this.width;
 	this.flatThickness = 0.2 * this.height;
@@ -38,6 +42,76 @@ Shelf.prototype.makeFlatShelf = function () {
 	flatFrame.add(flat);
 
 	return flatFrame;
+}
+
+Shelf.prototype.range = function () {
+  var framePos = this.frame.position;
+  var minX = framePos.x - 0.5 * this.width;
+  var maxX = framePos.x + 0.5 * this.width;
+  var minY = framePos.y - 0.5 * this.flatThickness;
+  var maxY = framePos.y + 0.5 * this.flatThickness;
+  var minZ = framePos.z - 0.5 * this.depth;
+  var maxZ = framePos.z + 0.5 * this.depth;
+
+  return {'minX': minX, 'maxX': maxX, 'minY': minY, 'maxY': maxY, 'minZ': minZ, 'maxZ': maxZ};
+}
+
+Shelf.prototype.addIntersectedBook = function (bookIndex) {
+  if (!this.intersectsBook(bookIndex)) {
+    return;
+  }
+
+  var book = this.floor[bookIndex];
+  var bookPos = book.frame.position;
+  var shelfRange = this.range();
+
+  //set the x coordinate
+  var x = bookPos.x;
+  if (x < shelfRange.minX) {
+    x = shelfRange.minX;
+  }
+  if (x > shelfRange.minX) {
+    x = shelfRange.maxX;
+  }
+
+  //set the y coordinate
+  var y = range.minY + 0.5 * this.flatThickness + 0.5 * book.height;
+
+  //set the z coordinate
+  var z = range.minZ + 0.5 * this.depth + 0.5 * book.depth;
+
+  book.frame.position.set(x, y, z);
+  book.rotation.y = Math.PI/2;
+
+  this.floor.splice(bookIndex, 1);
+  this.books.push(book);
+}
+
+Shelf.prototype.intersectsBook = function (bookIndex) {
+  var book = this.floor[bookIndex];
+  if (!book) {
+    return false;
+  }
+
+  var shelfRange = this.range();
+  var bookRange = book.range();
+
+  var xTolerance = 0.5 * book.width;
+  var xMinOkay = bookRange.minX >= shelfRange.minX - xTolerance && bookRange.minX <= shelfRange.maxX + xTolerance;
+  var xMaxOkay = bookRange.maxX >= shelfRange.minX - xTolerance && bookRange.maxX <= shelfRange.maxX + xTolerance;
+  var xOkay = xMinOkay || xMaxOkay;
+
+  var yTolerance = 0.5 * book.height;
+  var yMinOkay = bookRange.minY >= shelfRange.minY - yTolerance && bookRange.minY <= shelfRange.maxY + yTolerance;
+  var yMaxOkay = bookRange.maxY >= shelfRange.minY - yTolerance && bookRange.maxY <= shelfRange.maxY + yTolerance;
+  var yOkay = yMinOkay || yMaxOkay;
+
+  var zTolerance = 0.5 * book.depth;
+  var zMinOkay = bookRange.minZ >= shelfRange.minZ - zTolerance && bookRange.minZ <= shelfRange.maxZ + zTolerance;
+  var zMaxOkay = bookRange.maxZ >= shelfRange.minZ - zTolerance && bookRange.maxZ <= shelfRange.maxZ + zTolerance;
+  var zOkay = zMinOkay || zMaxOkay;
+
+  return xOkay && yOkay && zOkay;
 }
 
 Shelf.prototype.clearBooks = function (objectsControls) {
